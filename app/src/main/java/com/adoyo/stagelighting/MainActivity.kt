e6e2428959df36b797dfe6ed5f7dea8659d1cde4
AdoyoClifford <adoyoclifford@gmail.com>
2023-02-20 09:04:28 +0300

    initial commit

--- app/src/main/java/com/adoyo/stagelighting/MainActivity.kt
+++ app/src/main/java/com/adoyo/stagelighting/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.adoyo.stagelighting.presentation.TopBar
 import com.adoyo.stagelighting.ui.theme.StageLightingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                    // MainScreen(items = items, onItemClick = {})
+                    TopBar()
                 }
             }
         }
